8ea2f0c8f96c3db2360568841dd97e6ddd954a14
Leonardo Gomes Ribeiro <leoribeiro120710@gmail.com>
2026-09-14 12:45:16 -0300

    versao 1.5

---
+++ lib/tela_iniciar.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class TelaIniciar extends StatefulWidget {
+  const TelaIniciar({super.key});
+
+  @override
+  State<TelaIniciar> createState() => TelaIniciarState();
+}
--- lib/tela_jogo_heroi.dart
+++ lib/tela_jogo_heroi.dart
@@ -10,124 +10,210 @@ class TelaJogoHeroi extends StatefulWidget {
 class TelaJogoHeroiState extends State<TelaJogoHeroi> {
   String nomeHeroi = "";
   String titulo = "";
+  String classe = "";
+
   int vida = 0;
   int moedas = 0;
   int poder = 0;
   int inteligencia = 0;
-  String urlImagem = '';
+
+  String urlImagem = "";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: const Text("Jogo dos Heróis")),
+
       body: Center(
-        child: Column(
-          children: [
-            Text("Selecione seu Herói"),
-            Row(
-              children: [
-                ElevatedButton(
-                  onPressed: () => escolhaHeroi("Guerreiro"),
-                  child: Text("Guerreiro"),
-                ),
-                ElevatedButton(
-                  onPressed: () => escolhaHeroi("Ladino"),
-                  child: Text("Ladino"),
-                ),
-                ElevatedButton(
-                  onPressed: () => escolhaHeroi("Mago"),
-                  child: Text("Mago"),
-                ),
-              ],
-            ),
-            Image.network(urlImagem),
-            Card(
-              elevation: 5, // Dá uma sombra 3D ao cartão
-              color: Colors.grey[200],
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  children: [
-                    Text(
-                      'Classe: $nomeHeroi',
-                      style: const TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    'Classe: $nomeHeroi',
-                      style: const TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const Divider(),
-                    Text(
-                      'titulo: $titulo',
-                      style: const TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const Divider(), // Linha divisória
-                    Text(
-                      '❤️ Vida: $vida',
-                      style: const TextStyle(fontSize: 18, color: Colors.red),
-                    ),
-                    Text(
-                      '💰 Moedas: $moedas',
-                      style: const TextStyle(
-                        fontSize: 18,
-                        color: Colors.orange,
-                      ),
-                    ),
-                    Text(
-                      '⚔️ Poder: $poder',
-                      style: const TextStyle(fontSize: 18, color: Colors.blue),
-                    ),
-                    Text(
-                      '🧠 Poder: $inteligencia',
-                      style: const TextStyle(
-                        fontSize: 18,
-                        color: Color.fromARGB(255, 51, 255, 0),
-                      ),
-                    ),
-                  ],
-                ),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Selecione seu Herói",
+                style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
               ),
-            ),
-          ],
+
+              const SizedBox(height: 20),
+
+              // BOTÕES DOS HERÓIS
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () => escolhaHeroi("Guerreiro"),
+                    child: const Text("Guerreiro"),
+                  ),
+
+                  const SizedBox(width: 10),
+
+                  ElevatedButton(
+                    onPressed: () => escolhaHeroi("Ladino"),
+                    child: const Text("Ladino"),
+                  ),
+
+                  const SizedBox(width: 10),
+
+                  ElevatedButton(
+                    onPressed: () => escolhaHeroi("Mago"),
+                    child: const Text("Mago"),
+                  ),
+                ],
+              ),
+
+              const SizedBox(height: 30),
+
+              // IMAGEM DO HERÓI
+              if (urlImagem.isNotEmpty)
+                Image.network(
+                  urlImagem,
+                  width: 200,
+                  height: 250,
+                  fit: BoxFit.cover,
+
+                  errorBuilder: (context, error, stackTrace) {
+                    return const Icon(Icons.broken_image, size: 100);
+                  },
+                ),
+
+              const SizedBox(height: 20),
+
+              // CARD COM AS INFORMAÇÕES
+              if (nomeHeroi.isNotEmpty)
+                Card(
+                  elevation: 5,
+                  color: Colors.grey[200],
+
+                  child: Padding(
+                    padding: const EdgeInsets.all(20),
+
+                    child: Column(
+                      children: [
+                        // NOME
+                        Text(
+                          "Nome: $nomeHeroi",
+                          style: const TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+
+                        const SizedBox(height: 10),
+
+                        // TÍTULO
+                        Text(
+                          "Título: $titulo",
+                          style: const TextStyle(
+                            fontSize: 20,
+                            fontStyle: FontStyle.italic,
+                          ),
+                        ),
+
+                        const SizedBox(height: 10),
+
+                        // CLASSE
+                        Text(
+                          "Classe: $classe",
+                          style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+
+                        const Divider(),
+
+                        // VIDA
+                        Text(
+                          "❤️ Vida: $vida",
+                          style: const TextStyle(
+                            fontSize: 18,
+                            color: Colors.red,
+                          ),
+                        ),
+
+                        // MOEDAS
+                        Text(
+                          "💰 Moedas: $moedas",
+                          style: const TextStyle(
+                            fontSize: 18,
+                            color: Colors.orange,
+                          ),
+                        ),
+
+                        // PODER
+                        Text(
+                          "⚔️ Poder: $poder",
+                          style: const TextStyle(
+                            fontSize: 18,
+                            color: Colors.blue,
+                          ),
+                        ),
+
+                        // INTELIGÊNCIA
+                        Text(
+                          "🧠 Inteligência: $inteligencia",
+                          style: const TextStyle(
+                            fontSize: 18,
+                            color: Color.fromARGB(255, 51, 255, 0),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => TelaIniciar()),
+                  );
+                },
+                child: Text('Entrar'),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 
+  // FUNÇÃO PARA ESCOLHER O HERÓI
   void escolhaHeroi(String tipoHeroi) {
     setState(() {
       if (tipoHeroi == "Guerreiro") {
+        classe = "Guerreiro";
         nomeHeroi = "Jair M. Bolsonaro";
         titulo = "O patriota caído";
+
         vida = 200;
-        moedas = 22222;
+        moedas = 2222;
         poder = 70;
         inteligencia = 50;
+
         urlImagem =
-            "https://thumb.wikimedia.org/wikipedia/commons/thumb/3/37/Jair_Bolsonaro_2019_Portrait_%283x4_cropped_center%29.jpg/330px-Jair_Bolsonaro_2019_Portrait_%283x4_cropped_center%29.jpg?utm_source=pt.wikipedia.org&utm_campaign=parser&utm_content=thumbnail";
+            "https://thumb.wikimedia.org/wikipedia/commons/thumb/3/37/Jair_Bolsonaro_2019_Portrait_%283x4_cropped_center%29.jpg/330px-Jair_Bolsonaro_2019_Portrait_%283x4_cropped_center%29.jpg";
       } else if (tipoHeroi == "Ladino") {
+        classe = "Ladino";
         nomeHeroi = "Flávio Bolsonaro";
         titulo = "Son of Patriotsm";
+
         vida = 100;
-        moedas = 2222222;
+        moedas = 222222;
         poder = 150;
         inteligencia = -1;
+
         urlImagem =
             "https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/8d1a/live/f611e8b0-5566-11f1-ab55-37e8d4cd836a.jpg.webp";
       } else if (tipoHeroi == "Mago") {
+        classe = "Mago";
         nomeHeroi = "Daniel Vorcaro";
         titulo = "O mago do dinheiro";
+
         vida = 80;
-        moedas = 999999;
+        moedas = 99999999;
         poder = 300;
         inteligencia = 1000;
+
         urlImagem =
             "https://admin.cnnbrasil.com.br/wp-content/uploads/sites/12/2026/02/Daniel-Vorcaro.png?w=1000";
       }
